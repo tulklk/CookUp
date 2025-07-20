776f6767f9a357d8e865694597eceff7a16fe7fa
Phan Thanh Tu <tulklk32@gmail.com>
2025-07-21 02:31:57 +0700

    Add Joysstick Player Move

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,8 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private Rigidbody rb;
+
+    [Header("Movement Settings")]
+    public float speed = 5f;
+
+    [Header("Joystick Reference")]
+    public Joystick joystick;
+
+    private Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Lấy trục joystick: lên-xuống điều khiển trục X, trái-phải điều khiển trục Z
+        float moveX = joystick.Vertical;    // Đi tới/lùi
+        float moveZ = -joystick.Horizontal;  // Trái/phải
+
+        // Vector hướng di chuyển
+        movement = new Vector3(moveX, 0f, moveZ);
+
+        // Nếu có di chuyển thì quay Player theo hướng đó
+        if (movement.magnitude > 0.1f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            rb.rotation = Quaternion.Slerp(rb.rotation, toRotation, 0.2f);
+        }
+
+        // Di chuyển Player
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
 }
